beb2d03d6d9fb568bbc74bdafd913decc1048361
Peter Veres <ipuppyp@gmail.com>
2024-03-23 16:35:43 +0100

    update face api version

--- service/FacebookService.php
+++ service/FacebookService.php
@@ -11,7 +11,7 @@ class FacebookService {
         $this->facebookApi = new \Facebook\Facebook([
             'app_id' => $appId,
             'app_secret' => $appSecret,
-            'default_graph_version' => 'v12.0',
+            'default_graph_version' => 'v19.0',
             'default_access_token' => $accessToken
         ]);
         echo "Facebook service created, appId: $appId \n";
